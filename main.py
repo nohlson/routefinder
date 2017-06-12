86488d7031f53814be6d91b870cf1237a3589d91
medium shade <nohlson@purdue.edu>
2017-06-12 20:27:29 +0000

    fixed path to classes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request
-import sys
+import os, sys
+sys.path.append(os.getcwd() + "/classes")
 
-sys.path.append("classes")
 from model import Model
 
 app = Flask(__name__)
